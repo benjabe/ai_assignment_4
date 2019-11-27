2f930a01942943126e62a4f3bb169ddfb14b367f
Carl Findahl <sportleroro@gmail.com>
2019-11-27 10:15:18 +0000

    Create magazine class for handling ammo

---
+++ Assets/Scripts/Tanks/Ammo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Represents data about the ammo used by a Turret
+public class Ammo
+{
+    // Time in seconds between each shot
+    public float fireRate = 1.25f;
+
+    // Damage per shot
+    public int damage = 25;
+
+    public Ammo(float inFireRate, int inDamage)
+    {
+        fireRate = inFireRate;
+        damage = inDamage;
+    }
+}
